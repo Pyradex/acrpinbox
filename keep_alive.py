0978bc1383fa37cc26374dee7f470353ab7c7558
Pyradex <pyradex1@gmail.com>
2025-08-10 21:29:12 -0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,12 @@
 from flask import Flask
-from threading import Thread
+import os
 
-app = Flask(__name__)
+app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is alive!", 200
+    return "Bot is running!"
 
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    Thread(target=run).start()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
